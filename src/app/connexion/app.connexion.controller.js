6546bdde7560b8bbee6c2c59c2522e98997f3f47
SebastienLouis <sebastien.louis.pro@gmail.com>
2015-09-05 14:14:08 +0200

    forgotten message popup added

--- src/app/connexion/app.connexion.controller.js
+++ src/app/connexion/app.connexion.controller.js
@@ -5,38 +5,74 @@
     .module('app')
     .controller('ConnexionCtrl', ConnexionCtrl);
 
-    ConnexionCtrl.$inject = ['$log', '$scope', '$http'];
+    ConnexionCtrl.$inject = ['$log', '$scope', '$http', '$location'];
 
     /* @ngInject */
-    function ConnexionCtrl($log, $scope, $http) {
+    function ConnexionCtrl($log, $scope, $http, $location) {
+
+            $scope.userMail ="";
+            $scope.userPassword="";
+            var serverUrl = "https://pure-tor-1824.herokuapp.com";
+           
 
     		//Animation du panneau de connexion
     		$('#connexionPanel')
-    		.transition('hide')
+    		  .transition('hide')
  				.transition({
- 					animation : 'scale',
- 					duration   : '1s',
- 			});
+ 					animation : 'drop',
+ 					duration   : '1.3s',
+ 			    })
+            ;
 
 
  			//On connexionBtn click ==> Webservice connexion
     		$scope.connect = function(){
 
-    		$('#connexionBtn')
-                .transition('jiggle')
-            ;
-    			setTimeout(function(){
+    		  $('#connexionBtn')
+                .transition({
+                    animation : 'jiggle',
+                    duration : '0.5s'
+                });
+
+    	       setTimeout(function(){
                     //Stop loader and able inputs
                     startLoading();
                     
                 }, 800);
 
-    			setTimeout(function(){
-    				//Stop loader and able inputs
-    				stopLoading();
-    				
-    			}, 2000);
+    			
+
+                var data = {
+                    mail : $scope.userMail,
+                    password : $scope.userPassword
+                };
+
+
+                console.log(JSON.stringify(data));
+               $http.post(serverUrl+"/connexion", data).success(function(result){
+
+                if(result.mail){
+                    console.log(JSON.stringify(result));
+                    alert('connexion réussie');
+                    
+                }
+                   
+                else{
+                   alert('echec');
+                   console.log(JSON.stringify(result));
+                }
+                    
+                })
+
+
+                setTimeout(function(){
+                    //Stop loader and able inputs
+                    stopLoading();
+                    
+                }, 2000);
     		}
+
+
 
     		/*
     			startLoading() : Disable inputs and start loader inside connexion button
@@ -55,5 +91,16 @@
     			$('#password').attr("disabled", false);	
     			$("#connexionBtn").removeClass("loading");
     		}
+
+
+            $scope.showForgottenPassword = function(){
+                $('.forgottenPasswordDimmer').dimmer('show');
+            }
+
+            $scope.hideForgottenPassword = function(){
+                $('.forgottenPasswordDimmer').dimmer('hide');
+            }
+
+            
     	 }
 })();
