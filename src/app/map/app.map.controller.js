34e639c071b3ec744b89a5e27de55b3455c9249d
SebastienLouis <sebastien.louis.pro@gmail.com>
2015-09-11 15:34:20 +0200

    map service created and getContainers return mocks. Place markers in progress

--- src/app/map/app.map.controller.js
+++ src/app/map/app.map.controller.js
@@ -5,82 +5,123 @@
     .module('app')
     .controller('MapCtrl', MapCtrl);
 
-    MapCtrl.$inject = ['$log', '$scope', '$http'];
+    MapCtrl.$inject = ['$log', '$scope', '$http', 'mapService'];
 
     /* @ngInject */
-    function MapCtrl($log, $scope, $http, $location) {
+    function MapCtrl($log, $scope, $http, mapService) {
 
-
+        //Map object
         var map;
+        //To translate String adresse in Google Format adress with position (ex : 61.1648, 4.58058)
+        var geocoder = new google.maps.Geocoder();
+        // Markers object   
+        var markers ={};
             
-            /******************************  Mock datas  ******************************/
 
-/*
- Containers' mock datas
- */
+        function initializeContainers(){
+            $scope.containers = mapService.getContainers();
+            $log.debug('[MapCtrl] $scope.containers : ' + JSON.stringify($scope.containers)); 
+            for(var index in $scope.containers){
+                goPlaceMarker(index, $scope.containers[index]);
+            }
+        }
 
- $scope.containers = [{
- 'idContainer': 1234,
- 'name' : 'container 1',
- 'address': 'Palais Royal Paris',
- 'lat' : 48.858859,
- 'lng' : 2.347000,
- 'state': 1,
- 'lastCollect' : '2015-06-30 10:30:00',
- 'Errand_idErrand' : 1
- }, {
- 'idContainer': 1235,
- 'name' : 'container 2',
- 'address': 'Grenelle Paris',
- 'lat' : 48.858859,
- 'lng' : 2.347000,
- 'state': 1,
- 'lastCollect' : '2015-06-29 09:30:00',
- 'Errand_idErrand' : 1
- }, {
- 'idContainer': 1236,
- 'name' : 'container 3',
- 'address': 'Le marais Paris',
- 'lat' : 48.858859,
- 'lng' : 2.347000,
- 'state': 0,
- 'lastCollect' : '2015-06-28 08:30:00',
- 'Errand_idErrand' : 1
- }, {
- 'idContainer': 1237,
- 'name' : 'container 4',
- 'address': 'Val-de-grace Paris',
- 'lat' : 48.858859,
- 'lng' : 2.347000,
- 'state': 0,
- 'lastCollect' : '2015-06-27 07:30:00',
- 'Errand_idErrand' : 2
- }];
+        function initializeMap() {
+            // console.log('initializeMap called');
+            var mapOptions = {
+                zoom: 10,
+                center: new google.maps.LatLng(48.858859, 2.3470599),
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+            map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);        
+        }
+
+        function goPlaceMarker(i, container) {
+            setTimeout(function() {
+                placeMarker(container);
+                /*if(i == containers.length - 1){
+                    stopSpinner();
+                }*/
+
+            }, i * 500);
+        }
+
+        var placeMarker = function (container) {
+
+    // console.log('placeMarker called');
 
 
- /******************************  End of Mock datas  ******************************/
-
-
-function initializeMap() {
-
-    // console.log('initializeMap called');
-    var mapOptions = {
-        zoom: 10,
-        center: new google.maps.LatLng(48.858859, 2.3470599),
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+    var image = {
+        url: 'empty_container_marker.png'
     };
 
-    map = new google.maps.Map(document.getElementById('map_canvas'),
-        mapOptions);
+    if (container.state == 1 ) {
+        if (container.Errand_idErrand != 1)
+            image.url = 'busy_full_container_marker.png'
+        else
+            image.url = 'full_container_marker.png'
+    }
 
-    /*startSpinner();
-    for (container in containers) {
-        goPlaceMarker(container, containers[container]);   
-
-    }*/
-
+    else if(container.state == 2)
+    {
+     if (container.Errand_idErrand != 1)
+        image.url = 'busy_alert_container_marker.png'
+    else
+        image.url = 'alert_container_marker.png'
 }
-initializeMap();
+else if(container.Errand_idErrand != 1)
+    image.url = 'busy_empty_container_marker.png'
+
+
+geocoder.geocode({'address': container.address}, function (results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+
+        var newMarker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location,
+            clickable: true,
+            icon: image
+        });
+        newMarker.info = new google.maps.InfoWindow({
+            content: ""
+        });
+
+        newMarker.info.content += "Conteneur n&deg;" + container.idContainer + "<br>" + results[0].formatted_address;
+
+        if (container.Errand_idErrand != 1)
+            newMarker.info.content += "</br><span style=\" color : blue\">Appartient a la course n&deg;"+container.Errand_idErrand+"</span>";
+        else if (container.state == 1)
+            newMarker.info.content += "</br><span style=\" color : red\">Plein</span>";
+        else if (container.state == 0) 
+            newMarker.info.content += "</br><span style=\" color : green\">Vide</span>";
+        else if (container.state == 2) 
+            newMarker.info.content += "</br><span style=\" color : orange\">Alerte</span>";
+
+
+
+        if (container.Errand_idErrand != 1)
+            newMarker.info.content += "</br> <button disabled>Indisponible</button><br><br><div style='color:grey; font-size:10px; font-style:italic'>Derniere collecte : " + container.lastCollect + "</div>"
+        else if (container.isSelected == false)
+            newMarker.info.content += "</br> <button id=\"container" + container.idContainer + "\" onclick=\"addToErrand(" + container.idContainer + ")\">Ajouter a la course</button><br><br><div style='color:grey; font-size:10px; font-style:italic'>Derniere collecte : " + container.lastCollect + "</div>"
+        else
+            newMarker.info.content += "</br> <button disabled>Ajout&eacute;</button><br><br><div style='color:grey; font-size:10px; font-style:italic'>Derniere collecte : " + container.lastCollect + "</div>"
+
+
+        google.maps.event.addListener(newMarker, 'click', function () {
+            newMarker.info.open(map, newMarker);
+        });
+        markers[container.idContainer]=(newMarker);
+
+    } else {
+        // console.log('Geocode was not successful for the following reason: ' + status);
+    }
+
+})
+};
+
+        
+        initializeMap();
+        initializeContainers();
             
     	 }
 })();
---
+++ src/app/map/app.map.service.js
@@ -0,0 +1,80 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('mapService', mapService);
+
+    mapService.$inject = ['$log', '$http','$rootScope'];
+
+    function mapService($log, $http, $rootScope) {
+
+
+    	var serverUrl = $rootScope.serverUrl;
+    	var containers =[{
+ 'idContainer': 1234,
+ 'name' : 'container 1',
+ 'address': 'Palais Royal Paris',
+ 'lat' : 48.858859,
+ 'lng' : 2.347000,
+ 'state': 1,
+ 'lastCollect' : '2015-06-30 10:30:00',
+ 'Errand_idErrand' : 1
+ }, {
+ 'idContainer': 1235,
+ 'name' : 'container 2',
+ 'address': 'Grenelle Paris',
+ 'lat' : 48.858859,
+ 'lng' : 2.347000,
+ 'state': 1,
+ 'lastCollect' : '2015-06-29 09:30:00',
+ 'Errand_idErrand' : 1
+ }, {
+ 'idContainer': 1236,
+ 'name' : 'container 3',
+ 'address': 'Le marais Paris',
+ 'lat' : 48.858859,
+ 'lng' : 2.347000,
+ 'state': 0,
+ 'lastCollect' : '2015-06-28 08:30:00',
+ 'Errand_idErrand' : 1
+ }, {
+ 'idContainer': 1237,
+ 'name' : 'container 4',
+ 'address': 'Val-de-grace Paris',
+ 'lat' : 48.858859,
+ 'lng' : 2.347000,
+ 'state': 0,
+ 'lastCollect' : '2015-06-27 07:30:00',
+ 'Errand_idErrand' : 2
+ }];
+    	       /******************************  Mock datas  ******************************/
+ return {
+            getContainers: getContainers
+        };
+
+       
+
+        ////////////////
+
+
+
+         function getContainers (obj){
+
+              /*  if(obj){
+                    $http.get(serverUrl+'/containers', obj).success(function(result){
+                        return result;
+                    })
+                }
+
+                else{
+                     $http.get(serverUrl+'/containers').success(function(result){
+                      return result;
+                })
+                }*/
+               return containers;
+            }
+
+
+    }
+})();
