33185b593895a8a27f42ddbafe0c135782029481
Lyes Atek <lyes.atek@gmail.com>
2015-09-12 18:18:25 +0200

    Add update Container

--- src/app/containers/app.containers.controller.js
+++ src/app/containers/app.containers.controller.js
@@ -11,6 +11,17 @@
     function ContainerCtrl($log, $scope, $http, $rootScope) {
        //var serverUrl = 'http://localhost:8080';
        var serverUrl = $rootScope.serverUrl = "https://pure-tor-1824.herokuapp.com";
+
+       $scope.isEditableInfoView = false;
+       $scope.editInputs = function (){
+            $scope.isEditableInfoView = !$scope.isEditableInfoView;
+       };
+
+       $scope.closeContainerInfoView = function(){
+           $('#infoContainerModal')
+            .modal('hide'); 
+       };
+
        $scope.openContainerInfoView = function (selectedContainer){
             $('#infoContainerModal')
             .modal('setting','closable',false)
@@ -21,6 +32,21 @@
 
        };
 
+       $scope.updateContainer = function(){
+            $http.post(serverUrl+'/updateContainer', $scope.selectedContainer).success(function(result){
+                console.log($scope.selectedContainer._id);
+                console.log(JSON.stringify($scope.selectedContainer));
+                $scope.editInputs();
+                $scope.closeContainerInfoView();
+            })
+       }
+
+       $scope.deleteContainer = function(){
+            $http.post(serverUrl+'/deleteContainer', $scope.selectedContainer).success(function(result){
+                $scope.getContainers();
+                $scope.closeContainerInfoView;
+            })
+       }
        $scope.getContainers = function(id){
 
             if(id){
