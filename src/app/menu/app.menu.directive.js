eff0064adbac27fecf0e68c03a0851f8e46b57c1
SebastienLouis <sebastien.louis.pro@gmail.com>
2015-09-12 17:03:04 +0200

    menu directive update

--- src/app/menu/app.menu.directive.js
+++ src/app/menu/app.menu.directive.js
@@ -14,21 +14,10 @@
             scope : {},
             link : function(scope, element, attrs){
              scope.title = attrs.title;
-             scope.isNewErrandPopup = false;
 
              scope.openMenu = function(){
                 $('.ui.sidebar').sidebar('toggle')
-            };
-
-
-            scope.openOrCloseNewErrandPopup = function(){
-                scope.isNewErrandPopup = !scope.isNewErrandPopup;
-
-                if(scope.isNewErrandPopup)
-                    $('#newErrandDiv').hide();
-                else
-                    $('#newErrandDiv').show();
-            }
+            }; 
 
         }
     };
